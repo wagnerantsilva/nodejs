f8b6f06eeb8e704acd31373b954f464c5b4e2023
Tora Transp DEV <assinaturas.torausifast@gmail.com>
2019-09-24 07:37:11 -0300

    modify token

--- Slack/slack.js
+++ Slack/slack.js
@@ -3,7 +3,7 @@ const { SlackOAuthClient } = require('messaging-api-slack');
 const sendMessage = async (message, channel) =>{
 // get accessToken from facebook developers website
 const client = SlackOAuthClient.connect(
-    "xoxb-646245193670-645871248610-T8WpTMu3B1cj6wa5HWcpEkQA"
+    "YOUR_TOKEN"
   );
   
   client.postMessage(channel, message).then(() => {
